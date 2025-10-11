f928ac4268791d054d4ba5107ebdbd5344668875
Callum Watkins <callum@callumwatkins.com>
2025-10-11 22:58:38 +0100

    PyPI API type interfaces

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
+import { fileURLToPath } from "node:url";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxt/eslint"],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  alias: {
+    "@types": fileURLToPath(new URL("./types", import.meta.url)),
+  },
   compatibilityDate: "2025-07-15",
   eslint: {
     config: {
---
+++ types/pypi.ts
@@ -0,0 +1,78 @@
+type Nullable<T> = T | null;
+
+export interface PyPIResponse {
+  info: PackageInfo;
+  last_serial: number;
+  releases: Record<string, ReleaseFile[]>;
+  urls: ReleaseFile[];
+  vulnerabilities: Vulnerability[];
+}
+
+export interface PackageInfo {
+  author: Nullable<string>;
+  author_email: string;
+  bugtrack_url: Nullable<string>;
+  classifiers: string[];
+  description: string;
+  description_content_type: Nullable<string>;
+  docs_url: Nullable<string>;
+  download_url: Nullable<string>;
+  downloads: {
+    last_day: number;
+    last_month: number;
+    last_week: number;
+  };
+  dynamic: Nullable<string[]>;
+  home_page: Nullable<string>;
+  keywords: Nullable<string>;
+  license: Nullable<string>;
+  license_expression: Nullable<string>;
+  license_files: Nullable<string | string[]>;
+  maintainer: Nullable<string>;
+  maintainer_email: Nullable<string>;
+  name: string;
+  package_url: string;
+  platform: Nullable<string>;
+  project_url: string;
+  project_urls: Record<string, string>;
+  provides_extra: string[];
+  release_url: string;
+  requires_dist: string[] | null;
+  requires_python: Nullable<string>;
+  summary: string;
+  version: string;
+  yanked: boolean;
+  yanked_reason: Nullable<string>;
+}
+
+export interface ReleaseFile {
+  comment_text: Nullable<string>;
+  digests: {
+    blake2b_256: string;
+    md5: string;
+    sha256: string;
+  };
+  downloads: number;
+  filename: string;
+  has_sig: boolean;
+  md5_digest: string;
+  packagetype: string;
+  python_version: string;
+  requires_python: Nullable<string>;
+  size: number;
+  upload_time: string;
+  upload_time_iso_8601: string;
+  url: string;
+  yanked: boolean;
+  yanked_reason: Nullable<string>;
+}
+
+export interface Vulnerability {
+  id?: string;
+  details?: string;
+  aliases?: string[];
+  fixed_in?: string[];
+  withdrawn?: Nullable<string>;
+
+  [key: string]: unknown;
+}
